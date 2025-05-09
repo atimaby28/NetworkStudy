ad648582392c1efa7d4efc8b17a0147dd7a939f2
atimaby28 <atimaby28@gmail.com>
2025-05-09 21:46:44 +0900

    Update : Java/ChattingV3

---
+++ Java/chatting/src/server/command/JoinCommand.java
@@ -0,0 +1,22 @@
+package server.command;
+
+import server.Session;
+import server.SessionManager;
+
+import java.io.IOException;
+
+public class JoinCommand implements Command {
+
+    private final SessionManager sessionManager;
+
+    public JoinCommand(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @Override
+    public void execute(String[] args, Session session) throws IOException {
+        String username = args[1];
+        session.setUsername(username);
+        sessionManager.sendAll(username + "님이 입장하셨습니다.");
+    }
+}
