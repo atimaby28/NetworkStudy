38456cd9ef920aec1fb6481eeadaa62c0b441952
atimaby28 <atimaby28@gmail.com>
2025-05-11 14:53:34 +0900

    Update : Java/HttpServerV4

--- Java/httpserver/src/was/HttpServerV4.java
+++ Java/httpserver/src/was/HttpServerV4.java
@@ -8,12 +8,12 @@ import java.util.concurrent.Executors;
 
 import static util.MyLogger.log;
 
-public class HttpServerV3 {
+public class HttpServerV4 {
 
     private final int port;
     private final ExecutorService es = Executors.newFixedThreadPool(10);
 
-    public HttpServerV3(int port) {
+    public HttpServerV4(int port) {
         this.port = port;
     }
 
--- Java/httpserver/src/was/ServerMain.java
+++ Java/httpserver/src/was/ServerMain.java
@@ -7,7 +7,7 @@ public class ServerMain {
     private static final int PORT = 12345;
 
     public static void main(String[] args) throws IOException {
-        HttpServerV3 server = new HttpServerV3(PORT);
+        HttpServerV4 server = new HttpServerV4(PORT);
 
         server.start();
     }
