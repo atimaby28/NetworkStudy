56c72079af091adbaca4b11e2881f5d64272a6af
atimaby28 <atimaby28@gmail.com>
2025-05-15 00:15:22 +0900

    Update : Java/HttpServerAnnotationV3

--- Java/httpservletannotation/src/ServerMain.java
+++ Java/httpservletannotation/src/ServerMain.java
@@ -5,6 +5,7 @@ import was.httpserver.HttpServlet;
 import was.httpserver.ServletManager;
 import was.httpserver.servlet.DiscardServlet;
 import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.httpserver.servlet.annotation.AnnotationServletV3;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,7 +16,7 @@ public class ServerMain {
 
     public static void main(String[] args) throws IOException {
         List<Object> controllers = List.of(new SiteControllerV2(), new SearchControllerV2());
-        HttpServlet annotationServlet = new AnnotationServletV2(controllers);
+        HttpServlet annotationServlet = new AnnotationServletV3(controllers);
 
         ServletManager servletManager = new ServletManager();
         servletManager.setDefaultServlet(annotationServlet);
