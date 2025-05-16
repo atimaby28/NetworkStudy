60f8d231d5e2037491afb9bc1e7fb1f4abc4d239
atimaby28 <atimaby28@gmail.com>
2025-05-16 11:48:52 +0900

    Update : httpserver

---
+++ http/httpserver/src/ServerMain.java
@@ -0,0 +1,14 @@
+import was.HttpServer;
+
+import java.io.IOException;
+
+public class ServerMain {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = new HttpServer(PORT);
+
+        server.start();
+    }
+}
