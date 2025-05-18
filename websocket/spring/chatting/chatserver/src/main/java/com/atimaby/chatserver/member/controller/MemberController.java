ce02e4d65c2d4257aa763a70d09b9701f9e2eac1
atimaby28 <atimaby28@gmail.com>
2025-05-19 00:44:51 +0900

    Update : Security, Login

--- websocket/spring/chatting/chatserver/src/main/java/com/atimaby/chatserver/member/controller/MemberController.java
+++ websocket/spring/chatting/chatserver/src/main/java/com/atimaby/chatserver/member/controller/MemberController.java
@@ -1,22 +1,29 @@
 package com.atimaby.chatserver.member.controller;
 
+import com.atimaby.chatserver.common.auth.JwtTokenProvider;
 import com.atimaby.chatserver.member.domain.Member;
+import com.atimaby.chatserver.member.dto.MemberListResDto;
+import com.atimaby.chatserver.member.dto.MemberLoginReqDto;
 import com.atimaby.chatserver.member.dto.MemberSaveReqDto;
 import com.atimaby.chatserver.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
         this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/create")
@@ -24,5 +31,26 @@ public class MemberController {
         Member member = memberService.create(memberSaveReqDto);
 
         return new ResponseEntity<>(member.getId(), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/doLogin")
+    public ResponseEntity<?> doLogin(@RequestBody MemberLoginReqDto memberLoginReqDto) {
+        // email, password 검증
+        Member member = memberService.login(memberLoginReqDto);
+
+        // 일치할 경우 access token 발행
+        String jwtToken = jwtTokenProvider.createToken(member.getEmail(), member.getRole().toString());
+        Map<String, Object> loginInfo = new HashMap<>();
+        loginInfo.put("id", member.getId());
+        loginInfo.put("token", jwtToken);
+
+        return new ResponseEntity<>(loginInfo, HttpStatus.OK);
+    }
+
+    @GetMapping("list")
+    public ResponseEntity<?> memberList() {
+        List<MemberListResDto> dtos = memberService.findAll();
+
+        return new ResponseEntity<>(dtos, HttpStatus.OK);
     }
 }
--- websocket/spring/chatting/chatserver/src/main/java/com/atimaby/chatserver/member/service/MemberService.java
+++ websocket/spring/chatting/chatserver/src/main/java/com/atimaby/chatserver/member/service/MemberService.java
@@ -1,18 +1,27 @@
 package com.atimaby.chatserver.member.service;
 
 import com.atimaby.chatserver.member.domain.Member;
+import com.atimaby.chatserver.member.dto.MemberListResDto;
+import com.atimaby.chatserver.member.dto.MemberLoginReqDto;
 import com.atimaby.chatserver.member.dto.MemberSaveReqDto;
 import com.atimaby.chatserver.member.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Member create(MemberSaveReqDto memberSaveReqDto) {
@@ -24,11 +33,36 @@ public class MemberService {
         Member newMember = Member.builder()
                 .name(memberSaveReqDto.getName())
                 .email(memberSaveReqDto.getEmail())
-                .password(memberSaveReqDto.getPassword())
+                .password(passwordEncoder.encode(memberSaveReqDto.getPassword()))
                 .build();
 
         Member member = memberRepository.save(newMember);
 
         return member;
+    }
+
+    public Member login(MemberLoginReqDto memberLoginReqDto) {
+        Member member = memberRepository.findByEmail(memberLoginReqDto.getEmail()).orElseThrow(() -> new EntityNotFoundException("존재하지 않는 E-mail 입니다."));
+
+        if(!passwordEncoder.matches(memberLoginReqDto.getPassword(), member.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return member;
+    }
+
+    public List<MemberListResDto> findAll() {
+        List<Member> members = memberRepository.findAll();
+        List<MemberListResDto> memberListResDtos = new ArrayList<>();
+
+        for(Member m : members) {
+            MemberListResDto memberListResDto = new MemberListResDto();
+            memberListResDto.setId(m.getId());
+            memberListResDto.setName(m.getName());
+            memberListResDto.setEmail(m.getEmail());
+            memberListResDtos.add(memberListResDto);
+        }
+
+        return memberListResDtos;
     }
 }
