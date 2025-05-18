49b1a4c7d2b0f0b112c30cc300ad5d59b0425edb
atimaby28 <atimaby28@gmail.com>
2025-05-18 14:13:25 +0900

    Update : Create Member

---
+++ websocket/spring/chatting/chatserver/src/main/java/com/atimaby/chatserver/member/domain/Role.java
@@ -0,0 +1,5 @@
+package com.atimaby.chatserver.member.domain;
+
+public enum Role {
+    ADMIN, USER
+}
---
+++ websocket/spring/chatting/chatserver/src/main/java/com/atimaby/chatserver/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.atimaby.chatserver.member.repository;
+
+import com.atimaby.chatserver.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
